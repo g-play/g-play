741f6e8225ea20b3740c0667f4e9867f204b679e
Georgi Balabanov <88368087+git-Ball@users.noreply.github.com>
2023-04-12 17:54:37 +0300

    Show no games.

--- src/components/GameCatalog/GameCatalog.js
+++ src/components/GameCatalog/GameCatalog.js
@@ -2,14 +2,20 @@ import { useEffect, useState } from "react";
 import GameCard from "./GameCard";
 
 const GameCatalog = () =>{
-    const [games,setGames] =useState([])
+    const [games,setGames] =useState([]);
+    // const [loading,setLoading] = useState(false);
+
+    
     useEffect (()=>{
 setTimeout(()=>{
+    // setLoading(true);
     fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
 .then(res =>res.json())
 .then(result =>{
     // console.log(result)
     setGames(result)
+    // setLoading(false);
+
 })
 },2000)
     },[]);
@@ -26,9 +32,11 @@ setTimeout(()=>{
             </div>
 
         </div> */}
- {games.map(x=><GameCard game={x}/>)}
+ {games.length >0
+ ? games.map(x=><GameCard key={x._id} game={x}/>)
+ : <h3 className="no-articles">No articles yet</h3>
+}
 
-        <h3 className="no-articles">No articles yet</h3>
     </section>
     )
 }
